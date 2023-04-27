1fd876c6e9a5713581256a14f58474e78b945ab7
gyub <gyublayla@gmail.com>
2023-04-27 21:37:32 +0900

    임규빈: 230427 혼공자

---
+++ Ch06/src/sec02/FieldInitValue.java
@@ -0,0 +1,18 @@
+package sec02;
+
+public class FieldInitValue {
+
+	byte btyeField;
+	short shortField;
+	int intField;
+	long longField;
+	
+	boolean booleanField;
+	char charField;
+	
+	float floatField;
+	double doubleField;
+	
+	int[] arrField;
+	String referenceField;
+}
---
+++ Ch06/src/sec03/KoreanExample.java
@@ -0,0 +1,16 @@
+package sec03;
+
+public class KoreanExample {
+
+	public static void main(String[] args) {
+		Korean k1 = new Korean("박자바", "000000-1234567");
+		System.out.println("k1.name: "+k1.name);
+		System.out.println("k1.ssn: "+k1.ssn);
+		
+		Korean k2 = new Korean("김자바", "012345-7891234");
+		System.out.println("k2.name: "+k2.name);
+		System.out.println("k2.ssn: "+k2.ssn);
+
+	}
+
+}
