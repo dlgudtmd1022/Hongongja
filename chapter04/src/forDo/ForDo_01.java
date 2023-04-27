2acd6ce810839ea54c003531932fd71aa508e6fa
bitcamp1\bitcamp <chosy0716@gmail.com>
2023-04-27 19:46:41 +0900

    조승연 : 5챕터까지 완료

---
+++ chapter04/src/forDo/ForDo_01.java
@@ -0,0 +1,12 @@
+package forDo;
+
+public class ForDo_01 {
+
+	public static void main(String[] args) {
+		for(int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
---
+++ chapter04/src/forDo/ForDo_06.java
@@ -0,0 +1,14 @@
+package forDo;
+
+public class ForDo_06 {
+
+	public static void main(String[] args) {
+		int i = 1;
+		while(i <= 10) {
+			System.out.println(i);
+			i++;
+		}
+		
+	}
+
+}
