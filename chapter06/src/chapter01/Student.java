a94b570bc172aa115dcbfa2c26b3cf6cb890de77
dlgudtmd <dlgudtmd1022@gmail.com>
2023-04-28 11:48:39 +0900

    test

---
+++ chapter06/src/chapter01/Student.java
@@ -0,0 +1,12 @@
+package chapter01;
+
+public class Student {
+
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		System.out.println("s1 변수가 Student객체를 참조합니다.");
+		
+		Student s2 = new Student();
+		System.out.println("s2 변수가 또 다른Student객체를 참조합니다.");
+	}
+}
---
+++ chapter06/src/chapter01/클래스선언.java
@@ -0,0 +1,5 @@
+package chapter01;
+
+public class 클래스선언 {
+
+}
