3d2a9e4e254071d2e037f6d8b7e0e526017b10bc
gyub <gyublayla@gmail.com>
2023-05-01 19:52:36 +0900

    230501 혼공자 추가 업로드

---
+++ Ch11/src/sec01/ClassExample.java
@@ -0,0 +1,21 @@
+package sec01;
+
+public class ClassExample {
+
+	public static void main(String[] args) throws Exception{
+		
+		//Class clazz= Car.class;
+		
+		//Class clazz = Class.forName("sec01.exam08.Car");
+		
+		//Car car = new Car();
+	//	Class clzz = car.getClass();
+		
+	//	System.out.println(clazz.getName());
+	//	System.out.println(clazz.getSimpleName());
+	//	System.out.println(clazz.getPackage().getName());
+		
+
+	}
+
+}
