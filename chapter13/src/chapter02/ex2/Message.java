04bb47d3f7238f23497804186f292279f404bb15
dlgudtmd <dlgudtmd1022@gmail.com>
2023-05-02 15:38:17 +0900

    이형승 : chapter13

---
+++ chapter13/src/chapter02/ex2/Message.java
@@ -0,0 +1,12 @@
+package chapter02.ex2;
+
+public class Message {
+
+	public String command;
+	public String to;
+	
+	public Message(String command, String to) {
+		this.command = command;
+		this.to = to;
+	}
+}
